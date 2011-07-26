66a53e17811e57d592cdcb188d34a48db2c6cbcb
Steve Yen <steve.yen@gmail.com>
2011-07-26 11:42:40 -0700

    Update manifest to 1.1.2a branches

--- Manifest_VERSION.rb
+++ Manifest_VERSION.rb
@@ -16,5 +16,6 @@ VERSION_MOXI                     = "1.6.5"
 VERSION_LIBCONFLATE              = "1.6.5"
 VERSION_GROMMIT                  = "1.6.5"
 
-VERSION_COUCHDB  = "couchbase_1.1.1"
-VERSION_GEOCOUCH = "couchbase_1.1_geo"
+VERSION_COUCHDB  = "couchbase_1.1.2a"
+VERSION_GEOCOUCH = "couchbase_1.1.2a_geo"
+
