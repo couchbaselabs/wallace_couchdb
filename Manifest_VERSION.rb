066342a7c3761d4adeb6c2998108ae19f27554eb
Steve Yen <steve.yen@gmail.com>
2011-07-26 18:21:10 -0700

    1.1.2 Manifest_VERSION.rb

--- Manifest_VERSION.rb
+++ Manifest_VERSION.rb
@@ -16,6 +16,6 @@ VERSION_MOXI                     = "1.6.5"
 VERSION_LIBCONFLATE              = "1.6.5"
 VERSION_GROMMIT                  = "1.6.5"
 
-VERSION_COUCHDB  = "couchbase_1.1.2a"
-VERSION_GEOCOUCH = "couchbase_1.1.2a_geo"
+VERSION_COUCHDB  = "couchbase_1.1.2"
+VERSION_GEOCOUCH = "couchbase_1.1.2_geo"
 
